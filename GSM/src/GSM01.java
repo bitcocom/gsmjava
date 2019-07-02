1135d9943c3e5898bea386d9599336141a0d58e2
Park Mael IL <bitcocom@empas.com>
2019-07-02 15:18:25 +0900

    success

--- GSM/src/GSM01.java
+++ GSM/src/GSM01.java
@@ -1,7 +1,7 @@
 public class GSM01 {
 	public static void main(String[] args) {
        
-		System.out.println("apple\tapple");
+		System.out.println("Hello Java");
        
 	}
 }
---
+++ GSM/src/GSM02.java
@@ -0,0 +1,7 @@
+public class GSM02 {
+	public static void main(String[] args) {
+       int a;
+       a=10;
+       System.out.println(a);
+	}
+}
