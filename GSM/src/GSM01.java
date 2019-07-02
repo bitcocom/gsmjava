e915b1f79dc785101fbbb3874886df8ee706b4c3
Park Mael IL <bitcocom@empas.com>
2019-07-02 18:14:22 +0900

    success

--- GSM/src/GSM01.java
+++ GSM/src/GSM01.java
@@ -1,6 +1,17 @@
+import bit.gsm.BookVO;
 public class GSM01 {
 	public static void main(String[] args) {
         //syso + [Ctrl+SpaceBar]
-		System.out.println("Hello Java");		
+		//System.out.println("Hello Java");
+		// 변수, 자료형(DataType), 할당(=) : 프로그래밍의 3대요소
+		int a=10;
+		System.out.println(a);
+		
+		// 책(Object)->만들어 사용해라-->class
+		BookVO b=new BookVO();
+		b.setTitle("자바");
+		b.setPrice(12000);
+		b.setName("홍길동");
+		System.out.println(b.toString());
 	}
 }
---
+++ GSM/src/bit/gsm/BookVO.java
@@ -0,0 +1,35 @@
+package bit.gsm;
+// 책->상태(제목, 가격, 지은이, 출판사, 페이지, )+행위
+import java.lang.*;// 생략
+public class BookVO extends Object{
+    private String title;
+    private int price;
+    private String name;
+	public BookVO() {
+	   // 객체를 생성하는 동작 생략...
+	   super();// new Object()
+   }
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "BookVO [title=" + title + ", price=" + price + ", name=" + name + "]";
+	}
+	
+}
